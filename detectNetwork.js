0b7c9b13f9eb75fd9f721662fbf0a7241fdd9921
Elvin Kosova <elvinkosova@gmail.com>
2020-05-18 13:34:11 -0700

    Implements the frist step of the testbuilder

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,8 +1,8 @@
-// Given a credit card number, this function should return a string with the 
+// Given a credit card number, this function should return a string with the
 // name of a network, like 'MasterCard' or 'American Express'
 // Example: detectNetwork('343456789012345') should return 'American Express'
 
-// How can you tell one card network from another? Easy! 
+// How can you tell one card network from another? Easy!
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
@@ -13,6 +13,21 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  // Build our case structure
+  // JS objects return undefined for non-existing props by default
+  var result = {
+    '3814': "Diner's Club",
+    '3914': "Diner's Club",
+    '3415': "American Express",
+    '3715': "American Express"
+  };
+  // Extract first two digits and lenght
+  var prefix = cardNumber.substr(0, 2);
+  var length = cardNumber.length.toString();
+  // Build token
+  var token = prefix+length;
+  return result[token];
 };
 
 
